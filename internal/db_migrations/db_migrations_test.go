ceeefae53b3688250e5a5f2ca73266406c3bfac6
Juan Castrillon <castrillon.juan95@gmail.com>
2026-02-28 11:13:00 +0100

    Add past stats (no tested)

--- internal/db_migrations/db_migrations_test.go
+++ internal/db_migrations/db_migrations_test.go
@@ -80,3 +80,31 @@ func TestRunMigrations(t *testing.T) {
 		})
 	}
 }
+
+func TestSeedDataMigrations(t *testing.T) {
+	require := require.New(t)
+	db, err := sql.Open("sqlite3", ":memory:")
+	require.NoError(err)
+	err = RunMigrations(db, "migrations")
+	testCases := []struct {
+		Name        string
+		Query       string
+		QueryValues []any
+		Expected    string
+	}{
+		{
+			Name:        "Test a recipe in stats",
+			Query:       "SELECT efficiency FROM stats WHERE recipe_title == ?", // ('U2Fib3IgVHJvcGljYWw=', 1719180000, 16.4, 72) -- Sabor Tropical
+			QueryValues: []any{"U2Fib3IgVHJvcGljYWw="},
+			Expected:    "72",
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			var result string
+			require.NoError(db.QueryRow(tc.Query, tc.QueryValues...).Scan(&result))
+			require.Equal(tc.Expected, result)
+		})
+	}
+
+}
